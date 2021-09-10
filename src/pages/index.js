6a77432f8f0d7d20fbf77b4ab019783afcbd6037
Wojciech Kozieł <wojtek98koziel@gmail.com>
2021-09-10 16:52:45 +0200

    Router Setup

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import About from './AboutPage'
+import Auth from './AuthWrapper'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import Error from './ErrorPage'
+import Home from './HomePage'
+import PrivateRoute from './PrivateRoute'
+import Products from './ProductsPage'
+import SingleProducts from './SingleProductPage'
+
+export {
+  Home,
+  About,
+  Auth,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProducts,
+}
