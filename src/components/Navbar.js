daab206a3795c5fd023b4f39389a1d0981431da7
Wojciech Kozieł <wojtek98koziel@gmail.com>
2021-09-10 17:05:52 +0200

    Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,28 @@ import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
-  return <h4>navbar</h4>
+  return (
+    <NavContainer>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <Link to='/'>
+            <img src={logo} alt='comfy sloth' />
+          </Link>
+          <button type='button' className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          {links.map((link) => (
+            <li key={link.id}>
+              <Link to={link.url}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+        <CartButtons />
+      </div>
+    </NavContainer>
+  )
 }
 
 const NavContainer = styled.nav`
