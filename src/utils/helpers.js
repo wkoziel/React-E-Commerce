d35cb08f3cd3fc6ec6bd3c23823190945bdd63dd
Wojciech Kozieł <wojtek98koziel@gmail.com>
2021-09-19 14:36:30 +0200

    Featured products display

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  const price = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(number / 100)
+  return price
+}
 
 export const getUniqueValues = () => {}
