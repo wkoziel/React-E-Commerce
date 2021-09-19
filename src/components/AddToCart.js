f3833ff9b4aeaf07dff05a8d0abaa478eed0d1af
Wojciech Kozieł <wojtek98koziel@gmail.com>
2021-09-19 17:10:17 +0200

    Single product finished

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,8 +5,63 @@ import { FaCheck } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
-const AddToCart = () => {
-  return <h4>addToCart </h4>
+const AddToCart = ({ product }) => {
+  const { id, stock, colors } = product
+
+  const [mainColor, setMainColor] = useState(colors[0])
+  const [amount, setAmount] = useState(1)
+
+  const increaseAmount = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1
+      if (tempAmount > stock) {
+        tempAmount = stock
+      }
+      return tempAmount
+    })
+  }
+
+  const decreaseAmount = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1
+      if (tempAmount < 1) {
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+  }
+
+  return (
+    <Wrapper>
+      <div className='colors'>
+        <span> colors :</span>
+        <div>
+          {colors.map((color, index) => (
+            <button
+              key={index}
+              style={{ background: color }}
+              className={`${
+                mainColor === color ? 'color-btn active' : 'color-btn'
+              }`}
+              onClick={() => setMainColor(color)}
+            >
+              {mainColor === color ? <FaCheck /> : null}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className='btn-container'>
+        <AmountButtons
+          amount={amount}
+          decreaseAmount={decreaseAmount}
+          increaseAmount={increaseAmount}
+        />
+        <Link to='/cart' className='btn'>
+          Add to cart
+        </Link>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FaPlus, FaMinus } from 'react-icons/fa'
+import { FaPlus, FaMinus, FaCheck } from 'react-icons/fa'
 
-const AmountButtons = () => {
-  return <h4>amount buttons </h4>
+const AmountButtons = ({ increaseAmount, decreaseAmount, amount }) => {
+  return (
+    <Wrapper className='amount-btns'>
+      <button type='button' className='amount-btn' onClick={decreaseAmount}>
+        <FaMinus />
+      </button>
+      <h2 className='amount'>{amount}</h2>
+      <button type='button' className='amount-btn' onClick={increaseAmount}>
+        <FaPlus />
+      </button>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
